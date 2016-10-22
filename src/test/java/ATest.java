0de39c5cb37a750293bbc95b323eaaa7a4e30eaa
Nicolo <nicolo.ereni@satispay.com>
2016-10-22 13:13:38 +0200

    Initial commit

---
+++ src/test/java/ATest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ATest {
+
+    @Test
+    public void testName() throws Exception {
+        assertTrue(true);
+
+    }
+}
